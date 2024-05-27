629793cfbdb9550fca28f7e508692e425f2ec9ac
oja-bitterlife <bitter.life.club@gmail.com>
2024-05-27 22:21:07 +0900

    UI構築中

--- SaveLoad.py
+++ SaveLoad.py
@@ -26,6 +26,25 @@ class ARMATUE_SWITCHER_OT_load_settings(bpy.types.Operator):
         return{'FINISHED'}
 
 
+class ARMATUE_SWITCHER_OT_add(bpy.types.Operator):
+    bl_idname = "armature_switcher.add_bonemap"
+    bl_label = "Add BoneMap"
+
+    def execute(self, context):
+        context.scene.ARMATURE_SWITCHER_bonemap.add()
+        return{'FINISHED'}
+
+class ARMATUE_SWITCHER_OT_remove(bpy.types.Operator):
+    bl_idname = "armature_switcher.remove_bonemap"
+    bl_label = ""
+
+    id: bpy.props.IntProperty()
+
+    def execute(self, context):
+        context.scene.ARMATURE_SWITCHER_bonemap.remove(self.id)
+        return{'FINISHED'}
+
+
 
 class ARMATUE_SWITCHER_PT_setting(bpy.types.Panel):
     bl_label = "Settings"
@@ -42,26 +61,65 @@ class ARMATUE_SWITCHER_PT_setting(bpy.types.Panel):
                 module.draw(self, context, self.layout.box())
 
 
-        # Src側設定
-        src = self.layout.box()
-        src.label(text="Src (copy from)")
-        src.prop(context.scene, "ARMATURE_SWITCHER_src", text="Armature")
+        # Armature設定
+        box = self.layout.box()
+        box.label(text="Armature")
+        box.prop(context.scene, "ARMATURE_SWITCHER_armature_src", text="Src")
+        box.prop(context.scene, "ARMATURE_SWITCHER_armature_dist", text="Dist")
 
-        # Dist側設定
-        dist = self.layout.box()
-        dist.label(text="Dist (copy to)")
-        dist.prop(context.scene, "ARMATURE_SWITCHER_dist", text="Armature")
+        # Bone設定
+        box = self.layout.box()
+        box.label(text="Bone Mapping")
+        for i, bonemap in enumerate(context.scene.ARMATURE_SWITCHER_bonemap):
+            row = box.row()
+            row.prop(bonemap, "src_bone", text="")
+            row.label(icon="RIGHTARROW_THIN")
+            row.prop(bonemap, "dist_bone", text="")
 
+            # 閉じるボタン
+            row.operator("armature_switcher.remove_bonemap", icon="PANEL_CLOSE").id = i
+
+        box.operator("armature_switcher.add_bonemap")
 
         # Save/Loadボタン
-        self.layout.operator("armature_switcher.save_settings")
-        self.layout.operator("armature_switcher.load_settings")
+        row = self.layout.box().row()
+        row.operator("armature_switcher.save_settings")
+        row.operator("armature_switcher.load_settings")
 
 
 
 # セレクトボックスに表示したいArmatureのリストを作成する関数
-def get_armature_list(scene, context):
+def get_armature_list(self, context):
     return ((obj.name, obj.name, "") for obj in bpy.data.objects if obj.type == "ARMATURE" and obj.data.users > 0)
+
+# セレクトボックスに表示したいBoneのリストを作成する関数
+def get_src_bone_list(self, context):
+    armature_name = context.scene.ARMATURE_SWITCHER_armature_src
+    if(armature_name == None):
+        return ()
+
+    armature = bpy.data.objects[armature_name]
+    return ((bone.name, bone.name, "") for bone in armature.data.bones if bone.name)
+
+def get_dist_bone_list(self, context):
+    armature_name = context.scene.ARMATURE_SWITCHER_armature_dist
+    if(armature_name == None):
+        return ()
+
+    armature = bpy.data.objects[armature_name]
+    return ((bone.name, bone.name, "") for bone in armature.data.bones if bone.name)
+
+# ボーン対応表データ
+class BONE_MAP_DATA(bpy.types.PropertyGroup):
+    src_bone: bpy.props.EnumProperty(name="Src Bone", items=get_src_bone_list)
+    dist_bone: bpy.props.EnumProperty(name="Dist Bone", items=get_dist_bone_list)
+
+    def toJSON(self):
+        return json.dumps({
+            "src_bone": self.src_bone,
+            "dist_bone": self.dist_bone,
+        })
+
 
 def register():
     for module in modules:
@@ -69,8 +127,12 @@ def register():
             module.register()
 
     # ArmatureのSrc/DistのEnumPropertyを追加する
-    bpy.types.Scene.ARMATURE_SWITCHER_src = bpy.props.EnumProperty(items=get_armature_list)
-    bpy.types.Scene.ARMATURE_SWITCHER_dist = bpy.props.EnumProperty(items=get_armature_list)
+    bpy.types.Scene.ARMATURE_SWITCHER_armature_src = bpy.props.EnumProperty(name="Src Armature", items=get_armature_list)
+    bpy.types.Scene.ARMATURE_SWITCHER_armature_dist = bpy.props.EnumProperty(name="Dist Armature", items=get_armature_list)
+
+    # Boneの対応設定
+    bpy.types.Scene.ARMATURE_SWITCHER_bonemap = bpy.props.CollectionProperty(type=BONE_MAP_DATA)
+
 
 def unregister():
     for module in modules:
